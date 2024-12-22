925a1ca854bfa9000e269dc8e4b3bd96f2facfaf
AlWegner <alanwegnerm@gmail.com>
2024-12-22 02:12:37 -0300

    creando el app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+//Importar rutas
+//    ....    
+
+//Configurar Body Parser
+//    ....    
+
+//Configurar Static Folder
+//    ....    
+
+//Configurar CORS
+//    ....    
+
+//Configurar Rutas
+//    ....    
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+const app = require('./app.js');
 
+require('dotenv').config();
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 
-//Realizar la conexion
 const connectDB = async () => {
     try {
         await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/`);
